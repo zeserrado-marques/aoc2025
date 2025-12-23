43018d9bee0a29d3eb314d78221dcf8624b0288e
zeserrado-marques <zeserradommarques@gmail.com>
2025-12-23 21:29:19 +0000

    day7 completed after frustration and college deadlines

--- day7/second.py
+++ day7/second.py
@@ -24,10 +24,12 @@ with open(sys.argv[1]) as fh:
     while line := fh.readline():
         diagram.append(line.rstrip())
 
-
+# start variables
 res = 0
 first_beam = diagram[0].find(START)
 beam_indexes = [first_beam]
+ways_to_get = [0 for _ in range(len(diagram[0]))]
+ways_to_get[first_beam] = 1
 
 # regex to find all where splitters are
 pattern = re.compile(SPLITTER)
@@ -39,21 +41,23 @@ for i in range(1, len(diagram)):
     
     split_ids = get_splitter_indexes(diagram[i], pattern)
     
-    # print(diagram[i])
-    # print(split_ids)
-    
     new_beams = []
     for id in split_ids:
         if id in beam_indexes:
             new_beams += [id-1, id+1]
-            beam_indexes.remove(id) # because splitter is here, there's no new beam going through here
-    
-    # print(new_beams)
-    
+            ways_to_get[id-1] += ways_to_get[id]
+            ways_to_get[id+1] += ways_to_get[id] 
+            
+            ways_to_get[id] = 0
+            beam_indexes.remove(id)
+
     # new beams joining the club
     for beam in new_beams:
         if beam not in beam_indexes:
             beam_indexes.append(beam)
-            res+=1
     
-print(res)
+    # print(ways_to_get)
+
+# for path in paths:
+#     print(path, len(path))
+print(sum(ways_to_get))
